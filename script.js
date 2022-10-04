98e6729541ab7f890b93389ff21631096aafe1d0
Marissa Dilig <hello.maripd@gmail.com>
2022-10-04 07:50:44 -0500

    added randomWord function

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
-const wordArray = ['cat', 'dog', 'rat']
+///////////////////////////////////////////////////////////Global scope
+
 let lifeCount = 0
 const player = null
 const computer = null
 
 let userInputArray = []
+console.log(userInputArray.join(''))
 
 const submitBtn = document.querySelector('#submit-btn')
 const inputContainer = document.querySelector('.input-container')
 
-const getRandomNumber = () => {
-  let randomNum = Math.floor(Math.random() * wordArray.length)
-  console.log(randomNum)
-} //getRandomNumber is working
+const wordArray = ['cat', 'dog', 'rat', 'porcupine', 'fox']
 
-//create a function to combine letter strings together to make a word use concat()
+let randomWord = wordArray[Math.floor(Math.random() * wordArray.length)]
+console.log(randomWord)
 
-//create a function to get userInputValue
+/////////////////////////////////////////////////////////////Functions
+
 const getUserInput = () => {
-  //get the user input value
-  let userInputValue = document.querySelector('#letter-input').value
-  userInputArray.push(userInputValue)
-  // if user input value is a string
-  if (typeof userInputValue === 'string') {
+  const letterInput = document.querySelector('#letter-input')
+  let userInputValue = letterInput.value
+
+  console.log(userInputValue.length)
+  if (typeof userInputValue === 'string' && userInputValue.length > 0) {
     const pElement = document.createElement('p')
     pElement.innerText = userInputValue
+    userInputArray.push(userInputValue)
     document.querySelector('.input-container').append(pElement)
     document.getElementById('letter-input').value = null
     console.log(userInputArray)
   }
-  return
 }
-getUserInput()
 
+/////////////////////////////////////////////////////////////Event Listeners
 submitBtn.addEventListener('click', getUserInput)
