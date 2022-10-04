7dbc90eaafe18164cce0a99d44c29e50d609b999
Marissa Dilig <hello.maripd@gmail.com>
2022-10-04 14:54:49 -0500

    added win logic for guessing right letters or word

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
 ///////////////////////////////////////////////////////////Global scope
 
-let lifeCount = 0
+let lifeCount = 5
 const player = null
 const computer = null
 
 let userInputArray = []
-console.log(userInputArray.join(''))
 
+const headerText = document.getElementById('header-text')
 const submitBtn = document.querySelector('#submit-btn')
+const playGameBtn = document.querySelector('#playgame-btn')
 const inputContainer = document.querySelector('.input-container')
 
-const wordArray = ['cat', 'dog', 'rat', 'porcupine', 'fox']
-
+const wordArray = ['cat', 'dog', 'rat', 'fox']
 let randomWord = wordArray[Math.floor(Math.random() * wordArray.length)]
-console.log(randomWord)
 
 /////////////////////////////////////////////////////////////Functions
 
 const getUserInput = () => {
+  headerText.innerText = randomWord
   const letterInput = document.querySelector('#letter-input')
   let userInputValue = letterInput.value
 
-  console.log(userInputValue.length)
-  if (typeof userInputValue === 'string' && userInputValue.length > 0) {
+  if (randomWord.includes(userInputValue) && userInputValue.length > 0) {
+    userInputArray.push(userInputValue)
     const pElement = document.createElement('p')
     pElement.innerText = userInputValue
-    userInputArray.push(userInputValue)
     document.querySelector('.input-container').append(pElement)
-    document.getElementById('letter-input').value = null
-    console.log(userInputArray)
+    let joinLetters = userInputArray.join('')
+    letterInput.value = ''
+    console.log(joinLetters)
   }
+}
+getUserInput()
+
+const playGame = () => {
+  location.reload()
 }
 
 /////////////////////////////////////////////////////////////Event Listeners
 submitBtn.addEventListener('click', getUserInput)
+playGameBtn.addEventListener('click', playGame)
